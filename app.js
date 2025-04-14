f706e5a7edb503670f1f8391b1a24bef495a34a5
iannottaFabio <sfabio.iannotta@itis.pr.it>
2025-04-14 10:14:01 +0200

    rotta secure per con api

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import router from "./routes/PasswdRoute.js"
+import SecureRouter from "./routes/secureRoute.js";
 
 const app = express()
 app.use(express.json());
 
 app.use('/password', router)
+
+app.use('/secure', SecureRouter)
 
 app.listen(8080, () => {
     console.log("http://localhost:8080")
